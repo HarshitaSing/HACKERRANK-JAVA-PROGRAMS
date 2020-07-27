946c2fe847eb298b34e879965f42559e70eb8acc
Harshita Singh <30619470+HarshitaSing@users.noreply.github.com>
2020-07-27 22:38:53 +0530

    Add files via upload

---
+++ ADVANCED JAVA/Reflection API.java
@@ -0,0 +1,20 @@
+public class Solution {
+
+       public static void main(String[] args){
+            Student obj = new Student();
+            Class c4 = obj.getClass();
+            //ass student = Student.class;
+            Method[] methods = c4.getDeclaredMethods();
+
+            ArrayList<String> methodList = new ArrayList<>();
+            for(Method metho:methods)
+            {
+                methodList.add(metho.getName());
+            }
+            Collections.sort(methodList);
+            for(String name: methodList){
+                System.out.println(name);
+            }
+        }
+
+    }
